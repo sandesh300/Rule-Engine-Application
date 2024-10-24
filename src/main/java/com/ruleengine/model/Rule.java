8fc7e190e3c7ce2f6b98cfde40abc3cdb0a0fff4
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-24 16:15:19 +0530

    update the code

--- src/main/java/com/ruleengine/model/Rule.java
+++ src/main/java/com/ruleengine/model/Rule.java
@@ -1,14 +1,13 @@
 package com.ruleengine.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
